1b391e54d69b0a07173bae36368c5376d436c7bc
AshrafUzzaman04 <ashraf.uzzaman04082004@gmail.com>
2023-02-13 02:18:04 +0600

    event handler added for click to select data from the cart

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,10 +1,32 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
+import "./Products.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 const Products = (props) => {
-  //   console.log(props.allproduct.name);
+  const { name, img, seller, price, stock } = props.alldata;
   return (
     <div>
-      <h2>{props.allproduct.name}</h2>
+      <div className="product">
+        <div className="image">
+          <img src={img} alt="" />
+        </div>
+        <div className="pricing">
+          <h3>
+            <a href="#">{name}</a>
+          </h3>
+          <span>By: {seller}</span>
+          <br />
+          <br />
+          <span className="price">${price}</span>
+          <p>only {stock} left in stock- order soon</p>
+          <button onClick={() => props.handleAddProduct(props.alldata)}>
+            <FontAwesomeIcon icon={faShoppingCart} /> add to chart
+          </button>
+        </div>
+      </div>
+      <hr />
     </div>
   );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 import React, { useState } from "react";
 import fakeData from "../../fakeData/products.json";
 import Products from "../Products/Products";
@@ -5,24 +7,33 @@ import "./Shop.css";
 
 const Shop = () => {
   const first10 = fakeData.slice(0, 10);
-  const [porduct, setProduct] = useState(first10);
+  const [porducts, addProducts] = useState(first10);
+
+  // event handler
+  const handleAddProduct = (e) => {
+    console.log("Product added", e);
+  };
 
   return (
     <div className="shop_container">
       <div className="left_container">
-        <h1>Shop Now</h1>
-        {porduct.map((pro) => (
-          <Products allproduct={pro}></Products>
+        <h1>
+          <FontAwesomeIcon icon={faStar} style={{ color: "black" }} /> Shop Now
+        </h1>
+        {porducts.map((pd) => (
+          <Products alldata={pd} handleAddProduct={handleAddProduct}></Products>
         ))}
       </div>
       <div className="right_container">
-        <h2>Order Summary</h2>Lorem, ipsum dolor sit amet consectetur
-        adipisicing elit. Quibusdam delectus veritatis dolor dolorem illo
-        explicabo animi ducimus facere saepe quis? Est possimus magni
-        voluptatibus ipsum consequuntur sint quae aliquid deserunt dolore
-        delectus, optio nihil eveniet, sapiente ullam voluptates velit
-        recusandae reiciendis itaque? Repudiandae nulla illo blanditiis quaerat
-        delectus accusantium error.
+        <div className="all_data">
+          <h2>Order Summary</h2>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+          delectus veritatis dolor dolorem illo explicabo animi ducimus facere
+          saepe quis? Est possimus magni voluptatibus ipsum consequuntur sint
+          quae aliquid deserunt dolore delectus, optio nihil eveniet, sapiente
+          ullam voluptates velit recusandae reiciendis itaque? Repudiandae nulla
+          illo blanditiis quaerat delectus accusantium error.
+        </div>
       </div>
     </div>
   );
